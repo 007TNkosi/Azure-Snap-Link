3fb79d553a4a377cd434f27787ac3d97b35050d5
Thamsanqa <thamsanqa.nkosi@resbank.co.za>
2024-01-14 02:21:28 +0000

    Initial commit

---
+++ test_hello.py
@@ -0,0 +1,9 @@
+from hello import *
+
+def setup_function(function):
+    print("Running Setup: %s" % {function.__name__})
+    function.x = 10
+
+def teardown_function(function):
+    print("Running Teardown: %s" % {function.__name__})
+    del function.x

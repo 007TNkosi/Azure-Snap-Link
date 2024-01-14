ceb1fdf393b2d9634101cc545e31375d638e448b
Thami Nkosi <62917940+007TNkosi@users.noreply.github.com>
2024-01-14 04:39:21 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,5 @@
-from hello import *
+from hello import letStart
 
-def setup_function(function):
-    print("Running Setup: %s" % {function.__name__})
-    function.x = 10
-
-def teardown_function(function):
-    print("Running Teardown: %s" % {function.__name__})
-    del function.x
+def test_hello():
+    msg = "Goodbye"
+    assert letStart()

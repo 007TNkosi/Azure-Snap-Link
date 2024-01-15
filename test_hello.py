d4db528337c85a319d243611b4c5f5cccff46169
Thamsanqa <thamsanqa.nkosi@resbank.co.za>
2024-01-15 10:28:12 +0000

    Test Update #2

--- test_hello.py
+++ test_hello.py
@@ -2,8 +2,3 @@ from hello import letStart
 
 def test_hello():
     msg = "Goodbye"
-<<<<<<< HEAD
-    assert letStart()
-=======
-    assert letStart()
->>>>>>> 53267d19f62f650c990896a89976e74cd365e6eb
